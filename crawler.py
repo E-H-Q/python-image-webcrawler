1213ef3bc7e3e0a0cb2119e27d868e86c0d42fd4
Ben Zacharias <benzach0421@gmail.com>
2018-10-08 21:01:11 -0600

    Files are now numbered when saved.

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 url = "http://www.fiftythree.org/etherkiller/"
 
 def main(url: str):
+    i = 0
     image_regex = re.compile(".jpg$|.png$|.gif$")
     page_resp = requests.get(url)
     if not page_resp:
@@ -12,17 +13,19 @@ def main(url: str):
 
     html = BeautifulSoup(page_resp.text, "html5lib")
     images_found = html.find_all("a", {"href": image_regex})
+    length = len(images_found)
     if images_found:
         print ("Found",len(images_found),"images")
         for image in images_found:
-            save_image(image)
+            i += 1
+            save_image(image, i)
 
-def save_image(image):
+def save_image(image, i):
     image_resp = requests.get(f'{url}{image["href"]}')
     if not image_resp:
         print (image, image_resp.status_code)
     else:
-        filename = f"images/{image['href'].split('/')[-1]}"
+        filename = f"images/{i}- {image['href'].split('/')[-1]}"
         print (f"Saving {filename}")
         with open(filename, "wb") as image_file:
             image_file.write(image_resp.content)
